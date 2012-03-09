f8f578fa4129b47e810734a17a0c161bd393c97e
Konstantin Stepanov <me@kstep.me>
2012-03-09 19:05:53 +0300

    widget utils update
        * trim functions
        * humanize() returns multiple values, not table,
        * hms() renamed to dhms(), returns multiple values and include days,
        * hms is dhms now (days)

--- widgets/util.lua
+++ widgets/util.lua
@@ -1,11 +1,13 @@
+local math = math
+
 module('widgets.util')
 
-function hms(value)
+function dhms(value)
     local secs = value or 0
     local days  = math.floor(secs / 86400); secs = secs % 86400
     local hours = math.floor(secs / 3600); secs = secs % 3600
     local mins  = math.floor(secs / 60); secs = secs % 60
-    return { days, hours, mins, secs }
+    return days, hours, mins, secs
 end
 
 function humanize(value, meta)
@@ -19,5 +21,18 @@ function humanize(value, meta)
         value = value / scale
         suffix = suffix + 1
     end
-    return { value, suffixes[suffix] }
+    return value, suffixes[suffix]
 end
+
+function rtrim(value)
+    return value:gsub('%s+$', '')
+end
+
+function ltrim(value)
+    return value:gsub('^%s+', '')
+end
+
+function trim(value)
+    return rtril(ltrim(value))
+end
+

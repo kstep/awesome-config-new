efc57d256ff90fc89e28273841ad872f39fe525e
Konstantin Stepanov <me@kstep.me>
2012-03-09 18:46:35 +0300

    kbdd widget

---
+++ widgets/kbdd.lua
@@ -0,0 +1,31 @@
+
+local dbus = dbus
+local textbox = require('wibox.widget.textbox')
+local bg = require('wibox.widget.background')
+
+local setmetatable = setmetatable
+
+module('widgets.kbdd')
+
+local layouts = { [0] = ' En ', [1] = ' Ru ' }
+local colors = { [0] = '#0000ff', [1] = '#ff0000' }
+
+local widget = textbox()
+widget.bg = bg()
+widget.bg:set_fg('#ffffff')
+widget.bg:set_widget(widget)
+
+widget.update = function (self, layout)
+    self:set_text(layouts[layout] or ' ?? ')
+    self.bg:set_bg(colors[layout] or '#000000')
+end
+
+dbus.request_name("session", "ru.gentoo.kbdd")
+dbus.add_match("session", "interface='ru.gentoo.kbdd',member='layoutChanged'")
+dbus.connect_signal("ru.gentoo.kbdd", function (src, layout)
+    widget:update(layout)
+end)
+widget:update()
+
+setmetatable(_M, { __call = function (_, ...) return widget.bg end })
+

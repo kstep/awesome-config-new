e5fc1adef0d71824a2fac095473811b8c148d2a0
Konstantin Stepanov <me@kstep.me>
2012-03-09 18:54:57 +0300

    uptime widget

---
+++ widgets/uptime.lua
@@ -0,0 +1,37 @@
+local textbox = require('wibox.widget.textbox')
+local wutil = require('widgets.util')
+local timer = timer
+local io = io
+local setmetatable = setmetatable
+
+module('widgets.uptime')
+
+local function readfile(file, format)
+    local fh = io.open(file, 'r')
+    if fh then
+        local result = fh:read(format or '*a')
+        fh:close()
+        return result
+    end
+end
+
+function new()
+    local widget = textbox()
+    widget.update = function (self)
+        local uptime = readfile('/proc/uptime', '*n')
+        if uptime then
+            local d, h, m, s = wutil.dhms(uptime)
+            self:set_text((' [%dd %02d:%02d]'):format(d, h, m))
+        end
+    end
+    widget:update()
+
+    widget.timer = timer { timeout = 60 }
+    widget.timer:connect_signal('timeout', function () widget:update() end)
+    widget.timer:start()
+
+    return widget
+end
+
+setmetatable(_M, { __call = function (_, ...) return new(...) end })
+

c5b34a726ad9a181ad845ae9b9178ac523b7d319
Konstantin Stepanov <me@kstep.me>
2012-12-14 15:15:34 +0300

    theme update

--- theme.lua
+++ theme.lua
@@ -1,4 +1,5 @@
 theme = dofile("/usr/share/awesome/themes/sky/theme.lua")
-theme.wallpaper_cmd = { "awsetbg /home/kstep/.config/awesome/wallpaper.png" }
+--theme.wallpaper_cmd = { "awsetbg /home/kstep/.config/awesome/wallpaper.png" }
+theme.wallpaper = "/home/kstep/.config/awesome/wallpaper.png"
 theme.font = 'Terminus 8'
 return theme

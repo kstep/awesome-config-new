25a64b16cbd472a8b02be042cbc73c8ed715f326
Konstantin Stepanov <me@kstep.me>
2013-07-15 11:45:22 +0300

    remove unused layouts

--- rc.lua
+++ rc.lua
@@ -70,11 +70,8 @@ local layouts =
     awful.layout.suit.tile.left,
     awful.layout.suit.fair,
     awful.layout.suit.fair.horizontal,
-    awful.layout.suit.spiral,
-    awful.layout.suit.spiral.dwindle,
     awful.layout.suit.max,
     awful.layout.suit.max.fullscreen,
-    awful.layout.suit.magnifier
 }
 -- }}}
 

559a1018799157c36e383818acc3e23a8a3097ad
Konstantin Stepanov <me@kstep.me>
2016-01-25 14:13:03 +0300

    set layout via widget

--- widgets/kbdd.lua
+++ widgets/kbdd.lua
@@ -4,6 +4,7 @@ local textbox = require('wibox.widget.textbox')
 local bg = require('wibox.widget.background')
 local theme = require('beautiful')
 local util = require('awful.util')
+local button = require('awful.button')
 
 local setmetatable = setmetatable
 
@@ -17,6 +18,19 @@ widget.bg = bg()
 widget.bg:set_fg('#ffffff')
 widget.bg:set_widget(widget)
 
+local function set_layout(num)
+    util.spawn("/usr/bin/dbus-send --dest=ru.gentoo.KbddService /ru/gentoo/KbddService ru.gentoo.kbdd.set_layout uint32:" .. num)
+end
+local function switch_layout(dir)
+    util.spawn("/usr/bin/dbus-send --dest=ru.gentoo.KbddService /ru/gentoo/KbddService ru.gentoo.kbdd." .. dir .. "_layout")
+end
+
+widget:buttons(util.table.join(
+    button({ }, 1, function () switch_layout("next") end),
+    button({ }, 4, function () switch_layout("next") end),
+    button({ }, 5, function () switch_layout("prev") end)
+))
+
 widget.update = function (self, layout)
     self:set_text(layouts[layout] or ' ?? ')
     self.bg:set_bg(colors[layout] or theme.colors.base0)
@@ -27,8 +41,7 @@ dbus.add_match("session", "interface='ru.gentoo.kbdd',member='layoutChanged'")
 dbus.connect_signal("ru.gentoo.kbdd", function (src, layout)
     widget:update(layout)
 end)
---util.spawn("/usr/bin/dbus-send --dest=ru.gentoo.KbddService /ru/gentoo/KbddService ru.gentoo.kbdd.set_layout uint32:0")
-widget:update(0)
+set_layout(0)
 
 function kbdd.mt:__call(...)
     return widget.bg
